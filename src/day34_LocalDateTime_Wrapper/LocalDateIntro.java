26d89af5cdb9ce9839a0ba68b7695b4ad7ea2ae8
ViktoriiaMoskul <moskul1v@gmail.com>
2021-07-22 10:56:19 -0400

    LocalDate2

--- src/day34_LocalDateTime_Wrapper/LocalDateIntro.java
+++ src/day34_LocalDateTime_Wrapper/LocalDateIntro.java
@@ -8,5 +8,51 @@ public class LocalDateIntro {
 
         LocalDate today = LocalDate.now();
         System.out.println(today);
+
+        System.out.println("=========================");
+        //create array of string to store 5 classmates names
+        String[] names={"John","Daniel","Josh","Jimmy"};
+        LocalDate[] DoB={LocalDate.of(1990,5,25),
+                         LocalDate.of(1980,6,20),
+                         LocalDate.of(1985,7,28),
+                         LocalDate.of(1996,8,20)};
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]+"' birthday is "+DoB[i]);
+        }
+
+        System.out.println("======================");
+        LocalDate person1=LocalDate.of(1980,12,11);
+        LocalDate person2=LocalDate.of(1980,12,25);
+
+        if (person1.isBefore(person2)) {
+            System.out.println("person1 is older");
+        }else{
+            System.out.println("person2 is older");
+        }
+
+        if (person1.isAfter(person2)){
+            System.out.println("person2 is older");
+        }else{
+            System.out.println("person1 is older");
+        }
+
+        boolean r1 = person1.isLeapYear();
+        System.out.println(r1);
+
+        boolean r2 = LocalDate.of(2023,1,1).isLeapYear();
+        System.out.println(r2);
+        System.out.println("===================");
+        LocalDate currentDate = LocalDate.now();
+        System.out.println(currentDate);
+
+        LocalDate UnitAssessment3=currentDate.plusDays(14);
+        System.out.println(UnitAssessment3);
+
+        LocalDate unitAssessment4 = currentDate.plusMonths(1);
+        System.out.println(unitAssessment4);
+
+
+
     }
 }
